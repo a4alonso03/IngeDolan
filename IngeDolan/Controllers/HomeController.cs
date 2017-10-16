cb4977d24397de64fc57d938cd5dac350e784666
Julian Arguedas <julian.arguedastorres@gmail.com>
2017-10-16 03:39:40 -0600

    .

--- IngeDolan/Controllers/HomeController.cs
+++ IngeDolan/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using IngeDolan.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Web.Mvc;
 
 namespace IngeDolan.Controllers
@@ -10,6 +13,27 @@ namespace IngeDolan.Controllers
     {
         public ActionResult Index()
         {
+            /*
+            var auth = User.Identity.IsAuthenticated;
+            if (auth == true)
+            {
+                var name = User.Identity.Name;
+                var id = User.Identity.GetUserId();
+                using (ApplicationDbContext db = new ApplicationDbContext())
+                {
+                    var user = db.Users.Where(X => X.Id == id).FirstOrDefault();
+                    var confirmed = user.EmailConfirmed;
+
+                    var userManager = new UserManager<ApplicationUser>(
+                        new UserStore<ApplicationUser>(db));
+                    var uzer = new ApplicationUser();
+                    uzer.UserName = "admin";
+                    uzer.Email = "admin@defauil.com";
+
+                    var result = userManager.Create(uzer, "admin");
+                }
+            }
+            */
             return View();
         }
 
